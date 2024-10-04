551132c37ccc6f2cf8a93c27d8cf63d83bd9c991
Nilson Aanselmo <nilsonanselmo5@gmil.com>
2024-10-04 20:47:12 -0300

    criaçao de uma classe tarefa

---
+++ demo/src/test/java/com/erro4/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erro4.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
